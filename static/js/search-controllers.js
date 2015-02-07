9c71aecacf844f1ce3b3fb6383e8ddd579f35ad4
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-02-08 01:14:30 +0800

    clean up

--- static/js/search-controllers.js
+++ static/js/search-controllers.js
@@ -6,22 +6,21 @@
 
     var SITE_URL = "http://localhost:4567/";
 
-    // var inst_token = "292420707.661b085.14661ff4c6414d68ba1c583cbd7bac3c";
-
     var INST_API_URL = "https://api.instagram.com/v1";
 
-    var GG_TOKEN = "AIzaSyAs2yRcXM_Q_Ub8h5iTf5FP36f2RoiWO7Y";
+    var GOOG_TOKEN = "AIzaSyAs2yRcXM_Q_Ub8h5iTf5FP36f2RoiWO7Y";
 
     var app = angular.module('picspective.search-controllers', ['oauth-services']);
 
     app.controller('SearchController', ['$http', '$scope', 'searchDataService', function($http, $scope, sharedService) {
         var vm = this;
 
+        vm.query = {};
+        vm.query.hour = 3600*3;  // 3 hours
+        vm.count = 0;
+
         vm.finalQuery = finalQuery;
         vm.updateCnt = updateCnt;
-        vm.query = {};
-        vm.query.hour = 3600*3;  //
-        vm.count = 0;
 
         function finalQuery() {
 
@@ -86,7 +85,7 @@
         function loc2geocode(query, flag) {
             console.log(query);
             $http.get('https://maps.googleapis.com/maps/api/geocode/json?key='+
-            GG_TOKEN+"&address="+query.loc).success(function(data) {  // hack around
+            GOOG_TOKEN+"&address="+query.loc).success(function(data) {  // hack around
                 var geocode = data.results[0].geometry.location;  // lat, lng
                 //console.log("loc2geocode: ");
                 //console.log(geocode);
@@ -95,10 +94,7 @@
         }
 
         // instagram
-        var geocode_test = {
-            'lat': 1.3343083,
-            'lng': 103.7419582
-        };
+        var geocode_test = {'lat': 1.3343083, 'lng': 103.7419582};
         function instLocSearch(query, geocode, flag) {
             $http.jsonp(INST_API_URL+"/locations/search"+
             "?access_token="+oauthService.getAccessToken()+
@@ -116,7 +112,7 @@
         var geoid_test = {"meta":{"code":200},"data":[{"latitude":1.334249,"id":"1625905","longitude":103.741943,"name":"Boon Lay Raja Shark's Fin And Seafood Restaurant"},{"latitude":1.334374,"id":"1934957","longitude":103.742076,"name":"CW3@Jurong East Interchange Berth B7"},{"latitude":1.33432,"id":"47153690","longitude":103.741812,"name":"Kopitiam @ Jurong East Mrt"},{"latitude":1.33426999,"id":"536953444","longitude":103.742105104,"name":"Llao Llao Westmall"},{"latitude":1.334421,"id":"72127297","longitude":103.74207301,"name":"Bus CW3\/CW4 Lane To Tuas Checkpoint"},{"latitude":1.33425,"id":"346225591","longitude":103.7421131,"name":"Toastbox Jem"},{"latitude":1.3341477,"id":"485026021","longitude":103.7420142,"name":"New York Skin Solution Westgate"},{"latitude":1.334289,"id":"34027409","longitude":103.741786,"name":"Fiesta Jurong"},{"latitude":1.3341704,"id":"246334718","longitude":103.7420863,"name":"Sync"},{"latitude":1.334243417,"id":"20073477","longitude":103.74214232,"name":"Jurong Concession Card Replacement Office"},{"latitude":1.334129566,"id":"264611132","longitude":103.742095153,"name":"Capitamall Jurong East"},{"latitude":1.33442906,"id":"318770120","longitude":103.742157144,"name":"Jurong East Int, Cw3 Bus Queue"},{"latitude":1.33421747,"id":"300010705","longitude":103.742184845,"name":"west gate"},{"latitude":1.334112651,"id":"197558853","longitude":103.742105645,"name":"Singtel Shop"},{"latitude":1.334317545,"id":"305126794","longitude":103.741711842,"name":"Jurong Island"},{"latitude":1.334092626,"id":"265842323","longitude":103.742083479,"name":"Koi Cafe - Westgate"},{"latitude":1.334516048,"id":"32776626","longitude":103.742103577,"name":"Capricciosa"},{"latitude":1.334106477,"id":"495110318","longitude":103.74180447,"name":"Contours Express Ladies Gym Jurong East"},{"latitude":1.3341283,"id":"504197146","longitude":103.7421403,"name":"4 Fingers Jurong East"},{"latitude":1.334233864,"id":"279984898","longitude":103.742213169,"name":"Jurong East Westgate"}]};
 
         /**
-         *
+         * Need to wait for all promises
          * @param geoid
          * @param time in UNIX Timestamp
          * @param hour in UNIX Timestamp
