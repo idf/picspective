ee58683043e45f5babc62e1880f8ce37721d0d32
Raymond Jacobson <me@raymondjacobson.com>
2015-02-08 04:34:10 +0800

    add instagram oauth

--- static/js/search-controllers.js
+++ static/js/search-controllers.js
@@ -4,28 +4,23 @@
 (function(){
     'use strict';
 
-    var INST_TOKEN = "292420707.661b085.14661ff4c6414d68ba1c583cbd7bac3c";
     var INST_API_URL = "https://api.instagram.com/v1";
-    var GG_TOKEN = "AIzaSyAs2yRcXM_Q_Ub8h5iTf5FP36f2RoiWO7Y";
+    var GOOG_TOKEN = "AIzaSyAs2yRcXM_Q_Ub8h5iTf5FP36f2RoiWO7Y";
 
-    var app = angular.module('picspective.search-controllers', []);
+    var app = angular.module('picspective.search-controllers', ['oauth-services']);
 
     app.controller('SearchController', ['$http', '$scope', 'searchDataService', function($http, $scope, sharedService) {
         var vm = this;
 
-        vm.finalQuery = finalQuery;
-        vm.updateCnt = updateCnt;
         vm.query = {};
-        vm.query.hour = 3600*3;  //
+        vm.query.hour = 3600*3;  // 3 hours
         vm.count = 0;
 
+        vm.finalQuery = finalQuery;
+        vm.updateCnt = updateCnt;
+
         function finalQuery() {
-            sharedService.prepForBroadcast(vm.query);
-            $scope.$on('searchDataServiceReady', function() {
-                console.log('searchDataServiceReady');
-                vm.count = countMedia(sharedService);
-                vm.hour = sharedService.hour;
-            });
+
         }
 
         function updateCnt() {
@@ -45,7 +40,7 @@
     }]);
 
 
-    app.factory('searchDataService', ["$http", "$rootScope", "$q", function($http, $rootScope, $q) {
+    app.factory('searchDataService', ["$http", "$rootScope", "$q", "oauthService", function($http, $rootScope, $q, oauthService) {
         var sharedService = {};
         sharedService.prepForBroadcast = prepForBroadcast;
         sharedService.broadcastItem = broadcastItem;
@@ -54,6 +49,8 @@
         sharedService.broadcastCount = broadcastCount;
         sharedService.msg = [];
         sharedService.hour = 0;
+
+        oauthService.initialize("instagram");
 
         function broadcastItem() {
             console.log("broadcastItem");
@@ -66,37 +63,49 @@
         }
 
         function prepForBroadcast(query) {
-            loc2geocode(query, false);
+            loc2geocode(query, false);  // change to callback
         }
 
         function prepForCount(query) {
-            loc2geocode(query, true); // change to callback
+            if(oauthService.isReady()) {
+                loc2geocode(query, true);
+            }
+            else {
+                oauthService.connect("instagram").then(function () {
+                    loc2geocode(query, true);
+                });
+            }
         }
 
-        // HELPER FUNCTIONS
-        // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+
+        // HELPER funcitons
+        /**
+         * Googel Map service, location to geocode
+         * @param query
+         * @param flag
+         */
         function loc2geocode(query, flag) {
-            // console.log("loc2geocode: ");
-            // console.log(query);
+            console.log(query);
             $http.get('https://maps.googleapis.com/maps/api/geocode/json?key='+
-            GG_TOKEN+"&address="+query.loc).success(function(data) {  // hack around
-                // console.log("loc2geocode: ");
-                // console.log(data);
+            GOOG_TOKEN+"&address="+query.loc).success(function(data) {  // hack around
                 var geocode = data.results[0].geometry.location;  // lat, lng
-                // console.log("loc2geocode: ");
-                // console.log(geocode);
+                //console.log("loc2geocode: ");
+                //console.log(geocode);
                 instLocSearch(query, geocode, flag);
             });
         }
 
-        // instagram
-        var geocode_test = {
-            'lat': 1.3343083,
-            'lng': 103.7419582
-        };
+        var geocode_test = {'lat': 1.3343083, 'lng': 103.7419582};
+
+        /**
+         * Instagram location search
+         * @param query
+         * @param geocode
+         * @param flag
+         */
         function instLocSearch(query, geocode, flag) {
             $http.jsonp(INST_API_URL+"/locations/search"+
-            "?access_token="+INST_TOKEN+
+            "?access_token="+oauthService.getAccessToken()+
             "&lat="+geocode.lat+
             "&lng="+geocode.lng+
             "&distance=1000"+
@@ -111,7 +120,8 @@
         var geoid_test = {"meta":{"code":200},"data":[{"latitude":1.334249,"id":"1625905","longitude":103.741943,"name":"Boon Lay Raja Shark's Fin And Seafood Restaurant"},{"latitude":1.334374,"id":"1934957","longitude":103.742076,"name":"CW3@Jurong East Interchange Berth B7"},{"latitude":1.33432,"id":"47153690","longitude":103.741812,"name":"Kopitiam @ Jurong East Mrt"},{"latitude":1.33426999,"id":"536953444","longitude":103.742105104,"name":"Llao Llao Westmall"},{"latitude":1.334421,"id":"72127297","longitude":103.74207301,"name":"Bus CW3\/CW4 Lane To Tuas Checkpoint"},{"latitude":1.33425,"id":"346225591","longitude":103.7421131,"name":"Toastbox Jem"},{"latitude":1.3341477,"id":"485026021","longitude":103.7420142,"name":"New York Skin Solution Westgate"},{"latitude":1.334289,"id":"34027409","longitude":103.741786,"name":"Fiesta Jurong"},{"latitude":1.3341704,"id":"246334718","longitude":103.7420863,"name":"Sync"},{"latitude":1.334243417,"id":"20073477","longitude":103.74214232,"name":"Jurong Concession Card Replacement Office"},{"latitude":1.334129566,"id":"264611132","longitude":103.742095153,"name":"Capitamall Jurong East"},{"latitude":1.33442906,"id":"318770120","longitude":103.742157144,"name":"Jurong East Int, Cw3 Bus Queue"},{"latitude":1.33421747,"id":"300010705","longitude":103.742184845,"name":"west gate"},{"latitude":1.334112651,"id":"197558853","longitude":103.742105645,"name":"Singtel Shop"},{"latitude":1.334317545,"id":"305126794","longitude":103.741711842,"name":"Jurong Island"},{"latitude":1.334092626,"id":"265842323","longitude":103.742083479,"name":"Koi Cafe - Westgate"},{"latitude":1.334516048,"id":"32776626","longitude":103.742103577,"name":"Capricciosa"},{"latitude":1.334106477,"id":"495110318","longitude":103.74180447,"name":"Contours Express Ladies Gym Jurong East"},{"latitude":1.3341283,"id":"504197146","longitude":103.7421403,"name":"4 Fingers Jurong East"},{"latitude":1.334233864,"id":"279984898","longitude":103.742213169,"name":"Jurong East Westgate"}]};
 
         /**
-         *
+         * Query and store for each individual locaiton id
+         * Need to wait for all promises
          * @param geoid
          * @param time in UNIX Timestamp
          * @param hour in UNIX Timestamp
@@ -125,7 +135,7 @@
             sharedService.msg.length = 0; // clean  rather than reassign
             for(var i=0; i<geoid.data.length && i<UPPER; i++) {
                 promises.push($http.jsonp(INST_API_URL+"/locations/"+geoid.data[i].id+"/media/recent"+
-                "?access_token="+INST_TOKEN+
+                "?access_token="+oauthService.getAccessToken()+
                 "&min_timestamp="+start+
                 "&max_timestamp="+end+
                 "&callback=JSON_CALLBACK").success(function (data) {
@@ -136,6 +146,8 @@
                     }
                 }));
             }
+
+            // broadcasting
             $q.all(promises).then(function() {
                 if(flag) {
                     if(sharedService.msg.length>PIC) {
@@ -143,6 +155,7 @@
                         sharedService.broadcastItem();
                     }
                     else {
+                        // increase the hour range to 
                         recentMedia(geoid, time, hour*4, flag);
                     }
                 }
@@ -151,7 +164,6 @@
                 }
             });
         }
-
         return sharedService;
     }]);
 })();
