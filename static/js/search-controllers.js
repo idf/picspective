113a060645b434ff105bbe705c6c01544ba20a71
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-02-07 15:30:06 +0800

    dist

--- static/js/search-controllers.js
+++ static/js/search-controllers.js
@@ -3,14 +3,27 @@
  */
 (function(){
     'use strict';
+
+    var INST_TOKEN = "b3f8d7abdaf04e8f85ffeaf9ff4c96c7";
+    var INST_API_URL = "https://api.instagram.com/v1";
+    var GG_TOKEN = "AIzaSyAs2yRcXM_Q_Ub8h5iTf5FP36f2RoiWO7Y";
+
     var app = angular.module('picspective.search-controllers', []);
 
     app.controller('SearchController', ['$http', '$scope', 'searchDataService', function($http, $scope, sharedService) {
         var vm = this;
+        vm.searchQuery = searchQuery;
 
+        function searchQuery() {
+            sharedService.prepForBroadcast(vm.query);
+            $scope.$on('collocationDataReady', function() {
+                // do handling
+            });
+        }
     }]);
 
-    app.factory('searchDataService', ["$http", "$rootScope", function($http, $rootScope) {
+
+    app.factory('searchDataService', ["$http", "$rootScope", function($http, $rootScope, query) {
         var sharedService = {};
         sharedService.prepForBroadcast = prepForBroadcast;
         sharedService.broadcastItem = broadcastItem;
@@ -19,13 +32,35 @@
             $rootScope.$broadcast('searchDataServiceReady');
         }
 
-        function prepForBroadcast(str) {
-            $http.get('instagram'+str).success(function(data) {
-                console.log(data);
+        function prepForBroadcast(query) {
+            // google
+            // https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=API_KEY
+            function loc2geocode() {
+                $http.get('https://maps.googleapis.com/maps/api/geocode/json?key='+
+                GG_TOKEN+"&address="+query.loc).success(function(data) {
+                    console.log(data);
+                    var geocode = data.results[0].geometry.location;  // lat, lng
+                    console.log(geocode);
+                });
+            }
 
-            });
+            // instagram
+            var geocode = {
+                'lat': 1.3343083,
+                'lng': 103.7419582
+            };
+            function instLocSearch(geocode) {
+                $http.get(INST_API_URL+"/locations/search"+
+                    "?access_token="+INST_TOKEN+
+                    "&lat="+geocode.lat+
+                    "&lng="+geocode.lng
+                ).success(function(data) {
+                    console.log(data);
+                });
+            }
+
+            instLocSearch(geocode);
         }
-
         return sharedService;
     }]);
 })();
