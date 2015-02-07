91da894f5871b507c256aaf01e06c598143c08da
Yuan Yiyang <yuan3y@gmail.com>
2015-02-07 19:03:35 +0800

    clean up

--- static/js/share-controllers.js
+++ static/js/share-controllers.js
@@ -17,17 +17,6 @@
                 console.log(data);
                 var InstaImage = Parse.Object.extend("InstaImage");
                 var instaImage = new InstaImage();
-                // instaImage.set("provider_url", data.provider_url);
-                // instaImage.set("media_id", data.media_id);
-                // instaImage.set("author_name", data.author_name);
-                // instaImage.set("thumbnail_url", data.thumbnail_url);
-                // instaImage.set("height", data.height);
-                // instaImage.set("thumbnail_width", data.thumbnail_width);
-                // instaImage.set("thumbnail_height", data.thumbnail_height);
-                // instaImage.set("provider_name", data.provider_name);
-                // instaImage.set("", data.);
-                // instaImage.set("", data.);
-                // instaImage.set("", data.);
                 for (var key in data) {
                     if (!data.hasOwnProperty(key)) {
                         //The current property is not a direct property of p
